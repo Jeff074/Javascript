ba6b1a0c8fed3a990b0c796a6c4431585ed4f744
Jefferson Alves <93099418+Jeff074@users.noreply.github.com>
2023-03-26 19:15:11 -0300

    continuação do curso

---
+++ atribuicaoDeVariaveis.js
@@ -0,0 +1,13 @@
+console.log("Atribuicao de Valores");
+
+const primeiroNome = "Jefferson";
+const sobrenome = "Alves";
+
+console.log(primeiroNome , sobrenome);
+console.log(`Meo nome é ${primeiroNome} ${sobrenome}.`);
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + " " + sobrenome;
+console.log(nomeCompleto);
