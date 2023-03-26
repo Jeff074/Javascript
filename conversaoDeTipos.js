37cb6050c9436d171ff42a7723ac3f08dea56cad
Jefferson <Jeff074@outlook.com>
2023-03-26 10:04:20 -0300

    aulas Javascript alura

---
+++ conversaoDeTipos.js
@@ -0,0 +1,9 @@
+console.log("Conversão de Tipos");
+
+console.log("ano " +2023);
+console.log("2"+"2");
+console.log(parseInt("2")+parseInt("2"));
+console.log("10" / "2");
+console.log("Jefferson" / "2");//Not a Number
+console.log(6.5);
+console.log(6,5);
---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu primeiro programa: Trabalhando com variaveis");
+const idade = 32;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
+
+const idadadeSomada = idade +1;
+console.log(idadadeSomada);
+
